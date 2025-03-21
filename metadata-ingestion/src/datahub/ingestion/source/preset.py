4991030760c8549c9907987f571967b81d76a860
PeteMango <whcpeterwang@gmail.com>
2025-03-20 20:46:40 -0400

    removed logging

--- metadata-ingestion/src/datahub/ingestion/source/preset.py
+++ metadata-ingestion/src/datahub/ingestion/source/preset.py
@@ -112,7 +112,6 @@ class PresetSource(SupersetSource):
             }
         )
 
-        logger.info(f"\n\naccess token is: {self.access_token}\n\n")
         # Test the connection
         test_response = requests_session.get(f"{self.config.connect_uri}/version")
         if not test_response.ok:
